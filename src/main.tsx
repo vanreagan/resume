ac553259bdcd968da9f58989791e485df3ac05c3
vanreagan <103523086+vanreagan@users.noreply.github.com>
2026-02-09 17:48:58 -0600

    Replace App component with Document component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Document from "./Document.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Document />
   </StrictMode>,
-)
+);
