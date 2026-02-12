7a499e6235cc6b5b159a69fa93bdb67680628391
vanreagan <103523086+vanreagan@users.noreply.github.com>
2026-02-11 18:25:08 -0600

    Add dynamic title update and reorder components in Document

--- src/Document.tsx
+++ src/Document.tsx
@@ -3,10 +3,17 @@ import { Header } from "./components/Header";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Education } from "./components/Education";
+import { useEffect } from "react";
 
 const resume = resumeData;
 
 export default function Document() {
+  // Let's update the title dynamically for a cute touch <3
+
+  useEffect(() => {
+    document.title = `${resume.basics.name} - ${resumeData.basics.label} Resume`;
+  }, []);
+
   return (
     // A4/Letter Aspect Ratio Container: 8.5in x 11in = 204 x 264 tailwind units
     <div className="max-w-204 mx-auto min-h-264 bg-white p-12 shadow-xl print:shadow-none print:m-0 print:w-full print:p-0 flex flex-col gap-4">
@@ -14,9 +21,9 @@ export default function Document() {
 
       <Skills skills={resume.skills} />
 
-      <Projects projects={resume.projects} />
-
       <Education education={resume.education} />
+
+      <Projects projects={resume.projects} />
 
       <div className="fixed bottom-8 right-8 no-print">
         <button
