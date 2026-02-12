2d151babc0ba7ef5cb6bf27a91d4422e502815f3
vanreagan <103523086+vanreagan@users.noreply.github.com>
2026-02-11 18:45:46 -0600

    Add Experience component to Document for improved work history display

--- src/Document.tsx
+++ src/Document.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Education } from "./components/Education";
+import { Experience } from "./components/Experience";
 import { useEffect } from "react";
 
 const resume = resumeData;
@@ -18,11 +19,9 @@ export default function Document() {
     // A4/Letter Aspect Ratio Container: 8.5in x 11in = 204 x 264 tailwind units
     <div className="max-w-204 mx-auto min-h-264 bg-white p-12 shadow-xl print:shadow-none print:m-0 print:w-full print:p-0 flex flex-col gap-4">
       <Header basics={resume.basics} />
-
       <Skills skills={resume.skills} />
-
       <Education education={resume.education} />
-
+      <Experience work={resume.work} />
       <Projects projects={resume.projects} />
 
       <div className="fixed bottom-8 right-8 no-print">
